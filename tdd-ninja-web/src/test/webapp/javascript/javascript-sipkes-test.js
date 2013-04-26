005e196fd445812ce6a3052490823f72d4d9cef3
Pietro Martinelli <amicofragile@gmail.com>
2013-04-26 20:45:25 +0200

    SPIKE TEST != versus !==

--- tdd-ninja-web/src/test/webapp/javascript/javascript-sipkes-test.js
+++ tdd-ninja-web/src/test/webapp/javascript/javascript-sipkes-test.js
@@ -6,4 +6,12 @@ describe('ApplicationDescriptor tests', function () {
     it('=== comparison', function() {
             expect(1 === '1').not.toBe(true);
     });
+    
+    it('!= comparison', function () {  
+        expect(1 != '1').toBe(false);
+    });
+    
+    it('!== comparison', function () {  
+        expect(1 !== '1').toBe(true);
+    });
 });
